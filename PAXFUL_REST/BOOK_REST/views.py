f0e7b39cf184c57e6975cbab0760f6c7cc6efe91
abdul0214 <abdul.wahab0214@gmail.com>
2020-08-01 20:35:16 +0300

    Exception Handling added to Views.py

--- PAXFUL_REST/BOOK_REST/views.py
+++ PAXFUL_REST/BOOK_REST/views.py
@@ -1,8 +1,12 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from django.core import serializers
 from BOOK_REST.models import Book
 from BOOK_REST.serializers import BookSerializer
 from BOOK_REST.serializers import BookDetailSerializer
+from django.http import HttpResponse, Http404, HttpResponseBadRequest
 import sys
 
 
@@ -10,17 +14,34 @@ class BookList(APIView):
     """
     Retrieve the entire list of books
     """
-    def get(self, request):
+    def get_list(self):
         books = Book.objects.all()
-        serializer = BookSerializer(books,many=True)
-        return Response(serializer.data)
+        return books
+    def get(self, request):
+        try:
+            books = self.get_list()
+            serializer = BookSerializer(books,many=True)
+            if (len(books)!=0):
+                return Response(serializer.data,status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.data,status=status.HTTP_204_NO_CONTENT)
+        except:
+            print ("Unexpected error:", sys.exc_info()[0])
 
 
 class BookDetail(APIView):
     """
     Retrieve a product instance with details
     """
+    def get_object(self, pk):
+        try:
+            return Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            raise Http404
+        except:
+            print ("An Unexpected error occured:", sys.exc_info()[0])
+
     def get(self, request, pk, format=None):
-        book1 = Book.objects.get(pk=pk)
+        book1 = self.get_object(pk)
         serializer = BookDetailSerializer(book1)
         return Response(serializer.data)
