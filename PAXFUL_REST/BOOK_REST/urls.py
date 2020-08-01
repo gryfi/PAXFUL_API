45efc3f37d555996d9e4c67501b36a1db0f56daf
abdul0214 <abdul.wahab0214@gmail.com>
2020-08-02 01:14:57 +0300

    Added README.md and requirements.txt

--- PAXFUL_REST/BOOK_REST/urls.py
+++ PAXFUL_REST/BOOK_REST/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 urlpatterns = [
 
-path('/books/', views.BookList.as_view(),name='booklist'),
-path('/books/<int:pk>/', views.BookDetail.as_view(),name='bookdetail'),
+path('books/', views.BookList.as_view(),name='booklist'),
+path('books/<int:pk>/', views.BookDetail.as_view(),name='bookdetail'),
 ]
--- PAXFUL_REST/PAXFUL_REST/urls.py
+++ PAXFUL_REST/PAXFUL_REST/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin', admin.site.urls),
-    path('api/v1', include('BOOK_REST.urls')),
+    path('api/v1/', include('BOOK_REST.urls')),
 
 ]
